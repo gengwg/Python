95166986125a966fea8ac10127711318e5e0d5d0
Weigang Geng <gengwg@gmail.com>
2018-02-03 10:36:19 -0800

    find min according to specific key in list of dict.

---
+++ find_min_dict.py
@@ -0,0 +1,19 @@
+"""
+find min according to specific key in list of dict.
+"""
+portfolio = [
+   {'name': 'IBM', 'shares': 100, 'price': 91.1},
+   {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+   {'name': 'FB', 'shares': 200, 'price': 21.09},
+   {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+   {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+   {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+# generator argument
+min_shares = min(s['shares'] for s in portfolio)
+print min_shares
+
+# key argument
+min_shares = min(portfolio, key=lambda x: x['shares'])
+print min_shares
