55305ef0f8b34c98f24247977de8b4e0fdf42408
Weigang Geng <gengwg@gmail.com>
2018-02-04 23:49:30 -0800

    accurate decimal calculations

---
+++ decimal_calculations.py
@@ -0,0 +1,32 @@
+a = 4.2
+b = 2.1
+print a + b
+print (a + b) == 6.3
+
+from decimal import Decimal
+a = Decimal('4.2')
+b = Decimal('2.1')
+print (a + b)
+print (a + b) == Decimal('6.3')
+
+
+# control number of digits and rounding
+a = Decimal('1.3')
+b = Decimal('1.7')
+from decimal import localcontext
+print a / b
+with localcontext() as ctx:
+    ctx.prec = 3
+    print a / b
+with localcontext() as ctx:
+    ctx.prec = 50
+    print a / b
+
+# adding large and small numbers
+# output 0.0
+nums = [1.23e+18, 1 -1.23e+18]
+print sum(nums)
+import math
+print math.fsum(nums)
+nums = [1.23e+18, -1.23e+18, 1]
+print sum(nums) # 1.0
