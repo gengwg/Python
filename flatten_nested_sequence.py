7448f4de5b74763e76aabbe2aa11833fd412c1d0
Weigang Geng <gengwg@gmail.com>
2018-02-11 10:41:28 -0800

    flatten a nested sequence into a single list of values.

---
+++ flatten_nested_sequence.py
@@ -0,0 +1,20 @@
+from collections import Iterable
+
+# flatten a nested sequence into a single list of values.
+# recursive generator function involving yield from
+def flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from flatten(x)
+        else:
+            yield x 
+
+if __name__ == '__main__':
+    items = [1, 2, [3, 4, [5, 6], 7], 8]
+    for x in flatten(items):
+        print(x)
+
+    items = ['dave', 'wang', ['thomas', 'geng']]
+    print(list(flatten(items)))
+    for x in flatten(items):
+        print(x)
