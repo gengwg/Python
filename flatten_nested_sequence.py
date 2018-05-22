64d645c9f3f67ae737f56e2cda01d6b1eaf3710e
Weigang Geng <gengwg@gmail.com>
2018-05-21 22:33:46 -0700

    test cases

--- flatten_nested_sequence.py
+++ flatten_nested_sequence.py
@@ -7,10 +7,17 @@ def flatten(items, ignore_types=(str, bytes)):
         if isinstance(x, Iterable) and not isinstance(x, ignore_types):
             yield from flatten(x)
         else:
-            yield x 
+            yield x
 
 if __name__ == '__main__':
     items = [1, 2, [3, 4, [5, 6], 7], 8]
+    items = [
+      [1,2],
+      [3],
+      [],
+      [4,5,6],
+      []
+    ]
     for x in flatten(items):
         print(x)
 
