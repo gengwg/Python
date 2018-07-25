b1d4225f1c74222046e16e53d1c1cde6b122b3dd
Weigang Geng <gengwg@gmail.com>
2018-07-24 21:14:50 -0700

    carry state with callback functions.

---
+++ carry_state_with_callback_functions.py
@@ -0,0 +1,69 @@
+def apply_async(func, args, *, callback):
+    # compute the result
+    result = func(*args)
+
+    # invoke the callback with the result
+    callback(result)
+
+# single argument
+def print_result(result):
+    print('Got:', result)
+
+def add(x, y):
+    return x + y
+
+apply_async(add, (2, 3), callback=print_result)
+apply_async(add, ('hello', 'world'), callback=print_result)
+
+# use a bound-method instead of simple function
+class ResultHandler:
+    def __init__(self):
+        self.sequence = 0
+    def handler(self, result):
+        self.sequence += 1
+        print('[{}] Got: {}'.format(self.sequence, result))
+
+rh = ResultHandler()
+apply_async(add, (2, 3), callback=rh.handler)
+apply_async(add, ('hello', 'world'), callback=rh.handler)
+
+# use a closure to capture state
+def make_handler():
+    sequence = 0
+    def handler(result):
+        nonlocal sequence
+        sequence += 1
+        print('[{}] Got: {}'.format(sequence, result))
+    return handler
+
+handler = make_handler()
+apply_async(add, (2, 3), callback=handler)
+apply_async(add, ('hello', 'world'), callback=handler)
+
+# use a coroutine
+def make_handler():
+    sequence = 0
+    while True:
+        result = yield
+        sequence += 1
+        print('[{}] Got: {}'.format(sequence, result))
+
+handler = make_handler()
+next(handler)
+apply_async(add, (2, 3), callback=handler.send)
+apply_async(add, ('hello', 'world'), callback=handler.send)
+
+# use an extra argument and partial function
+class SequenceNo:
+    def __init__(self):
+        self.sequence = 0
+
+def handler(result, seq):
+    seq.sequence += 1
+    print('[{}] Got: {}'.format(seq.sequence, result))
+
+seq = SequenceNo()
+from functools import partial
+apply_async(add, (2, 3), callback=partial(handler, seq=seq))
+apply_async(add, ('hello', 'world'), callback=partial(handler, seq=seq))
+
