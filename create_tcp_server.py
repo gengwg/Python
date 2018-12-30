75d7cdaee3f65e44369303b2e91c5fe0b9c83f93
Weigang Geng <gengwg@gmail.com>
2018-12-29 23:33:41 -0800

    create a tcp server

---
+++ create_tcp_server.py
@@ -0,0 +1,58 @@
+from socketserver import BaseRequestHandler, TCPServer
+
+class EchoHandler(BaseRequestHandler):
+    def handle(self):
+        print('Got connection from', self.client_address)
+        while True:
+            msg = self.request.recv(8192)
+            if not msg:
+                break
+            self.request.send(msg)
+
+
+from socketserver import StreamRequestHandler, TCPServer
+
+
+class EchoHandler(StreamRequestHandler):
+    def handle(self):
+        print('Got connection from', self.client_address)
+        # self.rfile is a file-like object for reading
+        for line in self.rfile:
+            # self.wfile is a file-like object for writing
+            self.wfile.write(line)
+
+if __name__ == '__main__':
+    serv = TCPServer(('', 20000), EchoHandler)
+    serv.serve_forever()
+
+# implement tcp server directly using the socket library.
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+def echo_handler(address, client_sock):
+    print('Got connection from {}'.format(address))
+    while True:
+        msg = client_sock.recv(8192)
+        if not msg:
+            break
+        client_sock.sendall(msg)
+    client_sock.close()
+
+def echo_server(address, backlog=5):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(address)
+    sock.listen(backlog)
+    while True:
+        client_sock, client_addr = sock.accept()
+        echo_handler(client_addr, client_sock)
+
+if __name__ == '__main__':
+    echo_server(('', 20000))
+
+# test the server
+from socket import socket, AF_INET, SOCK_STREAM
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(('localhost', 20000))
+print(s.send(b'Hello'))
+print(s.recv(8192))
+
