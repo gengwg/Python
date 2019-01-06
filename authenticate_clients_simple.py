1db2a59ab0e54e2b8572bb46f553b818d35d54c3
Weigang Geng <gengwg@gmail.com>
2019-01-06 11:21:30 -0800

    authentication by a connection handshake using the hmac module.

---
+++ authenticate_clients_simple.py
@@ -0,0 +1,56 @@
+# authentication by a connection handshake using the hmac module.
+import hmac
+import os
+
+def server_authenticate(connection, secret_key):
+    message = os.urandom(32)
+    connection.send(message)
+    hash = hmac.new(secret_key, message)
+    digest = hash.digest()
+    response = connection.recv(len(digest))
+    return hmac.compare_digest(digest, response)
+
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+secret_key = b'peekaboo'
+
+def echo_handler(client_sock):
+    if not server_authenticate(client_sock, secret_key):
+        client_sock.close()
+        return
+    while True:
+        msg = client_sock.recv(8192)
+        if not msg:
+            break
+        client_sock.send(msg)
+
+def echo_server(address):
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(address)
+    s.listen(5)
+    while True:
+        c, a = s.accept()
+        echo_handler(c)
+
+echo_server(('', 18000))
+
+###### Client.py #######
+from socket import socket, AF_INET, SOCK_STREAM
+import hmac
+
+def client_authenticate(connection, secret_key):
+    message = connection.recv(32)
+    hash = hmac.new(secret_key, message)
+    digest = hash.digest()
+    connection.send(digest)
+
+
+secret_key = b'peekaboo'
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(('localhost', 18000))
+client_authenticate(s, secret_key)
+s.send(b'Hello')
+resp = s.recv(1024)
+print(resp)
