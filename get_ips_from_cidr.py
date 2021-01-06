d086fe0f87f19f9cf96a7769f039c9a602b691ed
gengwg <genwg@users.noreply.github.com>
2021-01-05 20:57:26 -0800

    Add example for IPv6; Use IP interface instead of CIDR

--- get_ips_from_cidr.py
+++ get_ips_from_cidr.py
@@ -13,9 +13,21 @@ Possible IPs for CIDR 10.65.36.23/29:
 10.65.36.21
 10.65.36.22
 10.65.36.23
+
+$ ./get_ips_from_cidr.py 12:3456:78:90ab:cd:ef01:23:30/125
+Possible IPs for CIDR 12:3456:78:90ab:cd:ef01:23:30/125:
+12:3456:78:90ab:cd:ef01:23:30
+12:3456:78:90ab:cd:ef01:23:31
+12:3456:78:90ab:cd:ef01:23:32
+12:3456:78:90ab:cd:ef01:23:33
+12:3456:78:90ab:cd:ef01:23:34
+12:3456:78:90ab:cd:ef01:23:35
+12:3456:78:90ab:cd:ef01:23:36
+12:3456:78:90ab:cd:ef01:23:37
 """
 
 # from netaddr import IPNetwork
+import ipaddress
 from ipaddress import ip_network
 import argparse
 
@@ -24,11 +36,20 @@ parser.add_argument("cidr")
 args = parser.parse_args()
 cidr = args.cidr
 
-print("Possible IPs for CIDR {}:".format(cidr))
-
 # Set strict to False: the host bits are masked out
 # to determine the appropriate network address.
 # Otherwise get error:
 #     ValueError: 10.65.36.23/29 has host bits set
-for ip in ip_network(cidr, False):
+net = ip_network(cidr, False)
+
+# An IP address and network address can be specified together as an IP interface.
+inet = ipaddress.ip_interface(cidr)
+net = inet.network
+
+print("INET: {}".format(cidr))
+print("CIDR: {}".format(net))
+print("IP ADDR: {}".format(inet.ip))
+print("Total number of IPs: {}".format(net.num_addresses))
+print("Possible IPs are:")
+for ip in net:
     print(ip)
