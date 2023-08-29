849f24cba09c4fca5f5b1e555674cca53a5744bc
W Geng <gengwg@users.noreply.github.com>
2023-08-29 10:28:44 -0700

    Update type_hints_optional.py

--- type_hints_optional.py
+++ type_hints_optional.py
@@ -1,6 +1,10 @@
 # Python is dynamically typed, so type hints are not enforced at runtime
 # but are meant for static analysis and code readability.
 
+# 1. Basic types like `str`, `int`, `float`, etc. don't require importing from `typing`.
+# 2. Complex types like `List`, `Tuple`, `Dict`, `Any`, `Union`, etc. 
+# should be imported from `typing` when used as type hints.
+    
 # To indicate that a variable or function parameter can be optional, use Optional from typing:
 from typing import Optional
 
