1c0cf6cc50ef7a36b4656cf6c6c93126c35db110
gengwg <gengwg@users.noreply.github.com>
2025-07-10 16:06:34 -0700

    more comments

--- string_representation_of_instances.py
+++ string_representation_of_instances.py
@@ -3,9 +3,12 @@ class Pair:
         self.x = x
         self.y = y
 
+    # The __repr__ method is used for official string representation
     def __repr__(self):
         return f"Pair({self.x!r}, {self.y!r})"
     
+    # The __str__ method is used for informal string representation
+    # If __str__ is not defined, Python will use __repr__ as a fallback
     def __str__(self):
         return f"({self.x!s}, {self.y!s})"
     
@@ -14,9 +17,11 @@ def main():
 
     print(f"p is {p!r}") # Calls __repr__
     print(f"p is {repr(p)}")  # Explicitly calls __repr__
+    print(f"p is {p.__repr__()}")  # Explicitly calls __repr__
 
     print(f"p is {p}")  # Calls __str__
-    print(f"p is {str(p)}")  # Explicitly calls __str__ 
-
+    print(f"p is {str(p)}")  # Explicitly calls __str__     
+    print(f"p is {p.__str__()}")  # Explicitly calls __str__
+    
 if __name__ == "__main__":
     main()
