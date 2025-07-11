6a14938fb6f4acc341c1861c53877aa8bae58ceb
gengwg <gengwg@users.noreply.github.com>
2025-07-11 11:24:31 -0700

    more comments

--- create_managed_properties.py
+++ create_managed_properties.py
@@ -2,7 +2,10 @@
 # only works in py3
 class Person:
     def __init__(self, first_name):
-        self.first_name = first_name
+        # In Python, when you define a property using the `@property` decorator along with a setter (using `@<property_name>.setter`), 
+        # assignments to that property (like `self.first_name = first_name`) will automatically call the setter method 
+        # instead of directly setting the attribute.
+        self.first_name = first_name 
 
     # getter function
     @property
@@ -21,13 +24,27 @@ class Person:
     def first_name(self):
         raise AttributeError('Cannot delete attribute')
 
-a = Person('Guido')
+a = Person('Guido') # calls the setter
 print(a.first_name) # calls the getter
-a.first_name = 'abc'
+
+a.first_name = 'Wei'    # calls the setter
 print(a.first_name)
-# a.first_name = 43   # calls the setter
-# del a.first_name
-# a = Person(43)
+
+try:
+    a.first_name = 43   # calls the setter
+except TypeError as e:
+    print(e)
+
+try:
+    b = Person(666)     # initialization also calls the setter
+except TypeError as e:
+    print(e)
+
+try:
+    del a.first_name  # calls the deleter
+except AttributeError as e:
+    print(e)
+
 
 import math
 class Circle:
@@ -43,6 +60,10 @@ class Circle:
         return 2 * math.pi * self.radius
 
 c = Circle(4.0)
-print(c.radius)
-print(c.area)
+print(c.radius) # calls the radius attribute directly
+print(c.area) # calls the area property. w/o @property, it would be a method call c.area().
+print(c.perimeter) # calls the perimeter property
+
+c.radius = 5.0 # changes the radius
+print(c.area) # calls the area property again
 print(c.perimeter)
