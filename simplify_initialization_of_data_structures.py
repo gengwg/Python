7c141c62a7952530092f2d02907dbb0361666d14
gengwg <genwg@users.noreply.github.com>
2025-07-14 10:46:41 -0700

    handle keyword arguments

--- simplify_initialization_of_data_structures.py
+++ simplify_initialization_of_data_structures.py
@@ -11,6 +11,23 @@ class Structure:
         for name, val in zip(self._fields, args):
             setattr(self, name, val)
 
+# handle keyword arguments
+class Structure2:
+    _fields = []
+    def __init__(self, *args, **kwargs):
+        if len(args) + len(kwargs) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # using setattr() to set attributes **dynamically**
+        for name, val in zip(self._fields, args):
+            setattr(self, name, val)
+
+        # handle keyword arguments
+        for name in kwargs:
+            if name not in self._fields:
+                raise TypeError('Unexpected keyword argument: {}'.format(name))
+            setattr(self, name, kwargs[name])
+
 # example class definitions
 if __name__ == '__main__':
     import math
@@ -32,5 +49,14 @@ if __name__ == '__main__':
     c = Circle(4.5)
     print(vars(c))
     print('area:', c.area())
-    s2 = Stock('ACME', 50) # TypeError
+    # s2 = Stock('ACME', 50) # TypeError
 
+
+    class Stock2(Structure2):
+        _fields = ['name', 'shares', 'price']
+
+    # s3 = Stock('ACME', 50, 91.1, 100) # TypeError
+    s4 = Stock2(name='ACME', shares=50, price=91.1) # works
+    print(vars(s4))
+    s5 = Stock2('ACME', 50, price=91.1) # works
+    print(vars(s5))
