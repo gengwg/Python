2afd92f3ef225fa042393568f0e39cf3fe9832a0
gengwg <gengwg@users.noreply.github.com>
2025-07-21 09:50:56 -0700

    demo weakref dict gc

--- cached_instance.py
+++ cached_instance.py
@@ -6,12 +6,13 @@ a = Spam('foo')
 b = Spam('bar')
 c = Spam('foo')
 
-print(a is b)
-print(a is c)
+print(a is b)   # False
+print(a is c)   # False
 
 import weakref
 _spam_cache = weakref.WeakValueDictionary()
 
+# factory function
 def get_spam(name):
     if name in _spam_cache:
         return _spam_cache[name]
@@ -26,3 +27,11 @@ c = get_spam('foo')
 
 print(a is b)  # False (different names)
 print(a is c)  # True (same name, cached instance)
+
+print(list(_spam_cache))    # ['foo', 'bar']
+del a 
+print(list(_spam_cache))    # ['foo', 'bar']
+del c
+print(list(_spam_cache))    # ['bar']
+del b 
+print(list(_spam_cache))    # []
